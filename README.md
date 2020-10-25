4d6cea83bfa28ae431ada83463f9ef5097a2c8ff
Duc Tran <1610800@hcmut.edu.vn>
2020-10-25 16:09:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Adaptive Weighting Mechanism for Reynolds Rules-based Flocking Control Scheme
+  
+## Prerequisite
+
+Run the simulation on ***Ubuntu 18.04***
+
+**Install Python3 pip**
+
+    $ sudo apt-get update
+    $ sudo apt-get install python3-pip
+
+**Install pygame**
+
+    $ pip3 install -U pygame --user
+
+## Instruction
+
+**Run the conventional method with obstacles**
+
+    $ cd conventional_method
+    $ python3 flock.py 1
+
+**Run the proposed method with obstacles**
+
+    $ cd proposed_method
+    $ python3 flock.py 1
+
+**Run without obstacles**
+
+    python3 flock.py
+
+**Usage**
+
+ - Right click to add obstacles
---
+++ plot.py
@@ -0,0 +1,51 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+if len(sys.argv)>1:
+    log = np.loadtxt( 'proposed_method/proposed_method.csv',delimiter = ',', max_rows=201)
+    log1= np.loadtxt( 'conventional_method/conventional_method.csv',delimiter = ',', max_rows=201)
+    x = [i for i in range(len(log))]
+    x1 = [i for i in range(len(log1))]
+    fig = plt.figure()
+
+# ###########################################################################################
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_yticks(np.arange(-1, 10, 1))
+    ax.set_xticks(np.arange(0, 200, 25), minor=True)
+    ax.grid(which='major', axis = 'y')
+    ax.tick_params(axis="both", labelsize=20)
+
+    plt.ylabel( 'Number of crashed drones',size = 26)
+    plt.xlabel( 'Iteration' ,size = 26)
+    plt.plot(x, log[:,1],'b^', label = 'Proposed Method', markersize=12)
+    plt.axis([0, 200, -2, 5], size = 26)
+    plt.legend(prop={"size":20})
+
+    plt.show()
+else:
+    log = np.loadtxt( 'proposed_method/proposed_method_no_ob.csv',delimiter = ',')
+    log1= np.loadtxt( 'conventional_method/conventional_method_no_ob.csv',delimiter = ',')
+    n = min(len(log),len(log1))
+    diff = [(log[i,0]- log1[i,0]) for i in range(n)]
+    x = [i for i in range(len(log))]
+    x1 = [i for i in range(len(log1))]
+    x2 = [i for i in range(n)]
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 2, 1)
+    ax.grid(which='both')
+    plt.ylabel( 'Average Distance (no obstacles)' )
+    plt.xlabel( 'Simulation' )
+    plt.plot(x, log[:,0],'y', label='dynamic')
+    plt.plot(x1, log1[:,0],'r', label = 'fixed')
+    plt.axis([0, 80, 0, 200])
+    plt.legend()
+
+    ax = fig.add_subplot(1, 2, 2)
+    ax.grid(which='both')
+    plt.ylabel( 'Average Distance Difference (no obstacles)' )
+    plt.xlabel( 'Simulation' )
+    plt.plot( x2, diff, label='difference')
+    plt.axis([0, 80, -100, 100])
+    plt.legend()
+    plt.show()
